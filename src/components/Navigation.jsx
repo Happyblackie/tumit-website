a996c36100f295d4c60e5162a0bbb5103641b406
Happyblackie <happyblackie01@gmail.com>
2026-05-08 14:49:53 +0300

    add guest login and session handling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 import NavLink from './NavLink';
 import Image from 'next/image';
+import getAuthUser from '@/lib/getAuthUser';
 
-export default function Navbar() {
+export default async function Navbar() {
+
+  const authUser = await getAuthUser();
+  // console.log(authUser);
+
   return (
     <div className="w-full max-w-5xl mx-auto p-4">
       <div className="flex flex-row justify-between items-center w-full gap-4">
@@ -20,15 +25,27 @@ export default function Navbar() {
         </Link>
 
         <nav className="flex items-center gap-8 text-sm">
-          <NavLink label="Dashboard" href="/dashboard"/>
-          <NavLink 
-            label={<><i className="bi bi-box-arrow-in-right mr-1"></i>Signup</>} 
-            href="/signup" 
-          />
-          <NavLink 
-            label={<><i className="bi bi-box-arrow-in-right mr-1"></i>Signin</>} 
-            href="/signin" 
-          />
+         
+            {authUser ? (
+              <div className="">
+                <NavLink label="Dashboard" href="/dashboard"/>
+              </div>
+            )
+            :(
+              <>
+                <NavLink 
+                  label={<><i className="bi bi-box-arrow-in-right mr-1"></i>Signup</>} 
+                  href="/signup" 
+                />
+                <NavLink 
+                    label={<><i className="bi bi-box-arrow-in-right mr-1"></i>Signin</>} 
+                    href="/signin" 
+                />
+                </>
+              
+            )}
+           
+                 
         </nav>
       </div>
     </div>

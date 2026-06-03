a0c785a0c2b75c42ffb810de343d29c334782d34
Happyblackie <happyblackie01@gmail.com>
2026-06-03 15:54:53 +0300

    Move Logo Further Left

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,7 +13,7 @@ export default async function Navbar() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white shadow-sm">
-      <div className="w-full max-w-5xl mx-auto p-4 py-3">
+      <div className="w-full max-w10xl mx-auto px-4 py-3">
         <div className="flex flex-row justify-between items-center w-full gap-4">
           
           <Link href="/" className="flex items-center">
